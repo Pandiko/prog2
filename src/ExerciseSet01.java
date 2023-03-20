d0064e72607a1d28c4a78fd4634b4f65ae6460d7
Pandiko <lucaknabe@gmail.com>
2023-03-20 12:35:34 +0100

    intial

---
+++ src/ExerciseSet01.java
@@ -0,0 +1,14 @@
+public class ExerciseSet01 {
+    public static void main(String[] args) {
+        System.out.println("00 01 02 03 04 05 06");
+        System.out.println("--------------------");
+        System.out.println(" .  .  .  .  .  .  .");
+        System.out.println(" .  .  .  .  .  .  .");
+        System.out.println(" .  .  .  .  .  .  .");
+        System.out.println(" .  .  .  .  .  .  .");
+        System.out.println(" .  .  .  .  .  .  .");
+        System.out.println(" .  .  .  .  .  .  .");
+        System.out.println("--------------------");
+    }
+
+}
